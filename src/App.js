3dff69b9255dca3a9ac5cbdb073c3d3af36fe10a
Garrett <pmcgarrett@gmail.com>
2021-12-03 10:37:37 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Beers from './components/Beers';
+import BeerDetails from './components/BeerDetails';
+import RandomBeer from './components/RandomBeer';
+import NewBeer from './components/NewBeer';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/beers" component={Beers} />
+        <Route exact path="/random-beer" component={RandomBeer} />
+        <Route exact path="/new-beer" component={NewBeer} />
+        <Route path="/:id" component={BeerDetails} />
+      </Switch>
+
+    </div>
+  );
+}
+
+export default App;
